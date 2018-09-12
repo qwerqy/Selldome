342a9fc8c14bc8672130515ff1dc8017fa14de3c
qwerqy <amnrsln@gmail.com>
2018-09-12 22:35:13 +0800

    adjusted routes, added root , override clearance routes , Oauth, etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resource :session, controller: "clearance/sessions", only: [:create]
 
-  resources :users, controller: "clearance/users", only: [:create] do
+  resources :passwords, controller: "passwords", only: [:create, :new]
+  resource :session, controller: "sessions", only: [:create]
+
+  resources :users, controller: "users", only: [:create, :update] do
     resource :password,
-      controller: "clearance/passwords",
+      controller: "passwords",
       only: [:create, :edit, :update]
   end
 
-  # get "/sign_in" => "clearance/sessions#new", as: "sign_in"
-  # delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
-  # get "/sign_up" => "clearance/users#new", as: "sign_up"
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+  get "/sign_in" => "sessions#new", as: "sign_in"
+  delete "/sign_out" => "sessions#destroy", as: "sign_out"
+  get "/sign_up" => "users#new", as: "sign_up"
+  get "/edit_user" => "users#edit", as: "edit_user"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Google OAuth Route
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 end
