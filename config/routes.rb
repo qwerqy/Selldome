78771f0eea2bfcade90db67a75796d8fc55e1f38
qwerqy <amnrsln@gmail.com>
2018-09-14 21:32:26 +0800

    added routes for listing & other fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,33 @@
 Rails.application.routes.draw do
 
   get 'welcome/index'
-  get 'home/index'
-
   root 'welcome#index'
 
+  get '/home' => 'home#index', as: "home"
 
+  # Listings Routes
+  resources :listings, controller: "listings", only: [:create]
+  get '/my-listings' => 'listings#index', as: "my_listings"
+  get '/my-listings/new' => 'listings#new', as: "new_listing"
+  post '/my-listing(.:format)' => 'listings#create'
+  get '/my-listing/:id/edit' => 'listing#edit', as: "edit_listing"
+
+  # Password Route
   resources :passwords, controller: "passwords", only: [:create, :new]
-  resource :session, controller: "sessions", only: [:create]
 
+  # Users Routes
   resources :users, controller: "users", only: [:create, :update] do
     resource :password,
       controller: "passwords",
       only: [:create, :edit, :update]
   end
-
-
-
-  get "/sign_in" => "sessions#new", as: "sign_in"
-  delete "/sign_out" => "sessions#destroy", as: "sign_out"
   get "/sign_up" => "users#new", as: "sign_up"
   get "/edit_user" => "users#edit", as: "edit_user"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Sessions Routes
+  resource :session, controller: "sessions", only: [:create]
+  get "/sign_in" => "sessions#new", as: "sign_in"
+  delete "/sign_out" => "sessions#destroy", as: "sign_out"
 
   # Google OAuth Route
   get "/auth/:provider/callback" => "sessions#create_from_omniauth"
