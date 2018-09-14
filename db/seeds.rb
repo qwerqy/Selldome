b6c53ed711003d2727a9d5097d0ef4c22e559328
qwerqy <amnrsln@gmail.com>
2018-09-14 21:32:50 +0800

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: 'Amin', last_name: 'Roslan', email: 'amnrsln@gmail.com', phone: '0123456789', password: 'amin123')
+
+Listing.create(
+  name: 'Studio Unit, M-City',
+  location: 'M-City, Jalan Ampang, Kuala Lumpur',
+  price: 1200,
+  description: "Like a megapolis of people and trees rising up in the air,
+  M City certainly makes an impression. Poised to be the next property hotspot,
+  M City will set a new benchmark as the most iconic tower in the Ampang area.
+  With parks, streams and sky bridges high up in the air,
+  the most exciting garden city development in Kuala Lumpur creates an all-new natural living experience.
+  Its location on the Embassy Row @ Jalan Ampang makes it particularly appealing to the thriving local and
+  international community. And by being just 5km away from KLCC, you won’t be far away from the epicentre of
+  Malaysia’s premier economic landmark.",
+  size: '1200sqf'
+)
