6b9243d6f33d66c201b125e2fa8097b0a641d651
qwerqy <amnrsln@gmail.com>
2018-09-16 05:08:33 +0800

    seed adjust

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ User.all.each { |u| uids << u.id }
 ActiveRecord::Base.transaction do
   40.times do
     listing['name'] = Faker::App.name
-    listing['place_type'] = rand(1..3)
+    listing['place_type'] = rand(1..7)
     listing['property_type'] = ["House", "Entire Floor", "Condominium", "Villa", "Townhouse", "Castle", "Treehouse", "Igloo", "Yurt", "Cave", "Chalet", "Hut", "Tent", "Other"].sample
 
     listing['room_number'] = rand(0..5)
