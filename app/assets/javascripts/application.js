7be5770c22a2919d329166f558ce294556e5218e
qwerqy <amnrsln@gmail.com>
2018-09-15 16:46:22 +0800

    deleted rails ujs cause it caused a bug which made jquery not working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,22 @@
 //
 //= require jquery3
 //= require popper
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+  $('#listing_place_type').change(function(){
+    let type = $(this).val();
+    if (type == 'Apartment') {
+      $('#listing_property_type').html(
+        "<option value=''>Select your Property Type</option><option value='Apartment'>Apartment</option><option value='Condominium'>Condominium</option><option value='Loft'>Loft</option><option value='Serviced Apartment'>Serviced Apartment</option>"
+      )
+    else if (type == 'House') {
+      $('#listing_property_type').html(
+        "<option value=''>Select your Property Type</option><option value='Apartment'>Apartment</option><option value='Condominium'>Condominium</option><option value='Loft'>Loft</option><option value='Serviced Apartment'>Serviced Apartment</option>"
+      )
+    }
+  })
+})
