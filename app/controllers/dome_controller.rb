1d89e5071c3f0df314b0394b5851f00ec9695669
qwerqy <amnrsln@gmail.com>
2018-09-17 00:58:53 +0800

    added helper to dome to avoid !login users to go

--- app/controllers/dome_controller.rb
+++ app/controllers/dome_controller.rb
@@ -1,4 +1,5 @@
 class DomeController < ApplicationController
+  before_action :require_login
   def index
     @listing = Listing.order(:created_at).page params[:page]
     render template: 'dome/index'
