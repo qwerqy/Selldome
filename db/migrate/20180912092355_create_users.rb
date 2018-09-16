37b310089a211b25d2c79e1433fb8e3f86726a60
qwerqy <amnrsln@gmail.com>
2018-09-17 00:48:29 +0800

    added columns for role

--- db/migrate/20180912092355_create_users.rb
+++ db/migrate/20180912092355_create_users.rb
@@ -9,6 +9,11 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.integer :gender
       t.string :phone
       t.string :country
+
+      t.boolean :superadmin, default: false
+      t.boolean :moderator, default: false
+      t.boolean :customer, default: true
+
       t.string :encrypted_password, limit: 128, null: false
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128, null: false
