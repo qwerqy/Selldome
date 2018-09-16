4f934338b979bce9bd0e436a430f00ce0b1c148a
qwerqy <amnrsln@gmail.com>
2018-09-17 04:31:06 +0800

    adjusted listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,16 +2,16 @@ class Listing < ApplicationRecord
   belongs_to :user
   acts_as_taggable
 
-  def place_type_string
-    if self.place_type == 1
+  def self.place_type_string(data)
+    if  data == 1
       "Apartment"
-    elsif self.place_type == 2
+    elsif  data == 2
       "House"
-    elsif self.place_type == 3
+    elsif  data == 3
       "Secondary Unit"
-    elsif self.place_type == 4
+    elsif  data == 4
       "Unique Space"
-    elsif self.place_type == 5
+    elsif data == 5
       "Boutique Hotel"
     else
       "Select One"
@@ -24,5 +24,13 @@ class Listing < ApplicationRecord
 
   def self.all_attr
     all_attr = ["Dates","Guests", "Home Type", "Price", "Instant Book", "More filters"]
+  end
+
+  def self.verified?(data)
+    if data == true
+      "Yes"
+    elsif data == false
+      "No"
+    end
   end
 end
