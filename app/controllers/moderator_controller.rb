9c7d53a778eafdbd3c42358d6cb59e57d17395a3
qwerqy <amnrsln@gmail.com>
2018-09-17 04:30:54 +0800

    moderator controller

--- app/controllers/moderator_controller.rb
+++ app/controllers/moderator_controller.rb
@@ -2,9 +2,19 @@ class ModeratorController < ApplicationController
   before_action :require_login
   def index
     if current_user.moderator? || current_user.superadmin?
-    render template: 'moderator/index'
+      @listing = Listing.order(:name).page params[:page]
+      render template: 'moderator/index'
+      respond_to do |format|
+        format.html
+        format.js
+      end
     else
-    redirect_to root_path
+      redirect_to root_path
     end
+  end
+
+  def pending_verification
+    @listing = Listing.order(:name).page params[:page]
+    render template: 'shared/pending-verification'
   end
 end
