6f345327f2186e217a83dc16ea92b50b7f0e93fe
qwerqy <amnrsln@gmail.com>
2018-09-17 00:59:16 +0800

    added role access to special panels

--- app/controllers/moderator_controller.rb
+++ app/controllers/moderator_controller.rb
@@ -1,5 +1,10 @@
 class ModeratorController < ApplicationController
+  before_action :require_login
   def index
+    if current_user.moderator? || current_user.superadmin?
     render template: 'moderator/index'
+    else
+    redirect_to root_path
+    end
   end
 end
--- app/controllers/superadmin_controller.rb
+++ app/controllers/superadmin_controller.rb
@@ -1,5 +1,10 @@
 class SuperadminController < ApplicationController
+  before_action :require_login
   def index
-    render template: 'superadmin/index'
+    if current_user.superadmin?
+      render template: 'superadmin/index'
+    else
+      redirect_to root_path
+    end
   end
 end
