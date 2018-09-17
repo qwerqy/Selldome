ff075642628004f37cac5304473df559f39c7ce1
qwerqy <amnrsln@gmail.com>
2018-09-17 21:56:12 +0800

    adjusted mod control for higher authorization and other fixes

--- app/controllers/moderator_controller.rb
+++ app/controllers/moderator_controller.rb
@@ -1,5 +1,7 @@
 class ModeratorController < ApplicationController
   before_action :require_login
+  skip_before_action :verify_authenticity_token, :only => [:verify]
+
   def index
     if current_user.moderator? || current_user.superadmin?
       @listing = Listing.order(:name).page params[:page]
@@ -9,23 +11,47 @@ class ModeratorController < ApplicationController
         format.js
       end
     else
+      flash[:danger] = "You have no access!"
       redirect_to root_path
     end
   end
 
   def pending_verification
-    @listing = Listing.order(:name).page params[:page]
-    render template: 'shared/pending-verification'
+    if current_user.moderator? || current_user.superadmin?
+      @listing = Listing.order(:name).page params[:page]
+      render template: 'shared/pending-verification'
+      respond_to do |format|
+        format.js
+      end
+    else
+      flash[:danger] = "You have no access!"
+      redirect_to root_path
+    end
   end
 
   def verify
-    @listing = Listing.find(params[:id])
-
-    @listing.update(verified: params[:verified])
+    if current_user.moderator? || current_user.superadmin?
+      @listing = Listing.find(params[:id])
+      @listing.update(verified: params[:verified])
+      respond_to do |format|
+        format.js { render 'shared/pending-verification'}
+      end
+    else
+      flash[:danger] = "You have no access!"
+      redirect_to root_path
+    end
   end
 
   def verified_listings
-    @listing = Listing.order(:name).page params[:page]
-    render template: 'shared/verified-listings'
+    if current_user.moderator? || current_user.superadmin?
+      @listing = Listing.order(:name).page params[:page]
+      render template: 'shared/verified-listings'
+      respond_to do |format|
+        format.js
+      end
+    else
+      flash[:danger] = "You have no access!"
+      redirect_to root_path
+    end
   end
 end
