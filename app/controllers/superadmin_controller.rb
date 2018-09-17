6c958bead7998793b1d85a07593cce407dbadaa4
qwerqy <amnrsln@gmail.com>
2018-09-17 21:56:29 +0800

    higher auth for superadmin controller

--- app/controllers/superadmin_controller.rb
+++ app/controllers/superadmin_controller.rb
@@ -1,14 +1,40 @@
 class SuperadminController < ApplicationController
   before_action :require_login
-  def index
-    if current_user.superadmin?
-      render template: 'superadmin/index'
-    else
-      redirect_to root_path
+    def index
+      if current_user.superadmin?
+        render template: 'superadmin/index'
+      else
+        flash[:danger] = "You have no access!"
+        redirect_to root_path
+      end
     end
-  end
 
-  def all_users
-    render template: 'shared/view-users'
-  end
+    def all_users
+      if current_user.superadmin?
+        render template: 'shared/view-users'
+      else
+        flash[:danger] = "You have no access!"
+        redirect_to root_path
+      end
+    end
+
+    def update_users
+      if current_user.superadmin?
+        render template: 'shared/update-users'
+      else
+        flash[:danger] = "You have no access!"
+        redirect_to root_path
+      end
+    end
+
+    def delete_user
+      if current_user.superadmin?
+        @user = User.find(params[:id])
+        @user.destroy
+        @user.listings.destroy_all
+      else
+        flash[:danger] = "You have no access!"
+        redirect_to root_path
+      end
+    end
 end
