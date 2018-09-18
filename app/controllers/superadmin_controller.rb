344df02cda72669e78c416957c47feb7c6dace5a
qwerqy <amnrsln@gmail.com>
2018-09-18 17:07:35 +0800

    superadmin has ability to edit user

--- app/controllers/superadmin_controller.rb
+++ app/controllers/superadmin_controller.rb
@@ -53,10 +53,47 @@ class SuperadminController < ApplicationController
       if current_user.superadmin?
         @user = User.find(params[:id])
         @user.destroy
-        redirect_to root_path
+        redirect_to admin_panel_path
       else
         flash[:danger] = "You have no access!"
-        redirect_to root_path
+        redirect_to admin_panel_path
       end
     end
+
+    def update_user
+      @user = User.find(params[:id])
+      render template: 'superadmin/edit-user'
+      respond_to do |format|
+        format.js
+      end
+    end
+
+    def confirm_update
+      if current_user.superadmin?
+        @user = User.find(params[:id])
+        @user.update_attributes(user_params)
+        if @user.save
+          flash[:warning] = "User has been edited"
+          redirect_to admin_panel_path
+        end
+      else
+        flash[:danger] = "You have no access!"
+        redirect_to admin_panel_path
+      end
+    end
+
+private
+
+  def user_params
+    params.require(:user)
+        .permit(
+          :first_name,
+          :last_name,
+          :country,
+          :gender,
+          :birthday,
+          :email,
+          :phone
+        )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,8 @@ Rails.application.routes.draw do
   get "/update-users" => "superadmin#update_users", as: "update_users"
   get "/delete/:id" => "superadmin#delete_user", as: "delete_user"
   delete "/delete/:id" => "superadmin#destroy_user", as: "destroy_user"
+  get "/update/:id" => "superadmin#update_user", as: "update_user"
+  patch "/update/:id" => "superadmin#confirm_update", as: "superadmin_user_edit"
 
   # Moderator
   get "/moderator-panel" => "moderator#index", as: "moderator_panel"
