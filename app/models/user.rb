d53517666a95ecdb329bc4021ab72dd984899c8d
qwerqy <amnrsln@gmail.com>
2018-09-19 15:08:16 +0800

    added carrierwaver file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   include Clearance::User
+  require 'carrierwave/orm/activerecord'
   mount_uploader :avatar, AvatarUploader
 
   has_many :authentications, dependent: :destroy
