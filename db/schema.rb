448d77bd35c25afbaa433385fc0c508f6bd232a5
qwerqy <amnrsln@gmail.com>
2018-09-19 16:59:12 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_19_041141) do
+ActiveRecord::Schema.define(version: 2018_09_19_085432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,15 @@ ActiveRecord::Schema.define(version: 2018_09_19_041141) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "photos"
     t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
