d68bea731d39544d17bf05cf6428d82e764d8126
qwerqy <amnrsln@gmail.com>
2018-09-23 00:30:19 +0800

    enable user to leave a review to a listing.

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,2 +1,33 @@
 class ReviewController < ApplicationController
+  def new
+    @listing = Listing.find(params[:listing_id])
+    @review = Review.new
+  end
+
+  def create
+    @listing = Listing.find(params[:listing_id])
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.listing_id = @listing.id
+    byebug
+    if @review.save
+      flash[:success] = "Thank you for the review!"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:danger] = "Uh Oh! Something's wrong!"
+      redirect_back(fallback_location: root_path)
+    end
+
+  end
+
+private
+
+  def review_params
+    params.require(:review).permit(
+      :rating,
+      :comment,
+      :listing_id,
+      :user_id
+    )
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :listing
+
+  validates :user_id, presence: true
+  validates :rating, presence: true
+
+  validates :listing_id, uniqueness: { scope: :user,
+    message: "User can only comment once" }
 end
