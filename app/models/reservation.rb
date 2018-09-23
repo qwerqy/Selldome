0bc2003a583a6a4dd9a8aa9a91c5aaf81766fdc1
qwerqy <amnrsln@gmail.com>
2018-09-23 22:13:52 +0800

    last one

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,5 +6,7 @@ class Reservation < ApplicationRecord
     message: "Date is taken" }
   validates :end_time , uniqueness: { scope: :user,
     message: "Date is taken" }
+  validates :listing_id , uniqueness: { scope: :user,
+    message: "You can't reserve a Home twice!" }
 
 end
