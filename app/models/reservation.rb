5186d0100c10c8ebc0a1ebbe4ea50cb9a085f92d
qwerqy <amnrsln@gmail.com>
2018-09-23 22:12:28 +0800

    add validation so user cant book the same date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,10 @@
 class Reservation < ApplicationRecord
   belongs_to :listing
   belongs_to :user
+
+  validates :start_time , uniqueness: { scope: :user,
+    message: "Date is taken" }
+  validates :end_time , uniqueness: { scope: :user,
+    message: "Date is taken" }
+
 end
