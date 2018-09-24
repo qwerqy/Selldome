90205a303d208c65453e0ea434339c2ef968d347
qwerqy <amnrsln@gmail.com>
2018-09-24 21:47:25 +0800

    add instant booking into column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_23_065445) do
+ActiveRecord::Schema.define(version: 2018_09_24_134450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_09_23_065445) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.json "photos"
+    t.boolean "instant_booking", default: false
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
