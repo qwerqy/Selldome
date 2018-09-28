77a375044a76ae30ee492413369310110f4a60a6
qwerqy <amnrsln@gmail.com>
2018-09-28 14:12:10 +0800

    Merge pull request #24 from qwerqy/feature/search
    Feature/search

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_25_081406) do
+ActiveRecord::Schema.define(version: 2018_09_28_032210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 2018_09_25_081406) do
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
   create_table "reservations", force: :cascade do |t|
     t.bigint "listing_id"
     t.bigint "user_id"
@@ -57,7 +66,7 @@ ActiveRecord::Schema.define(version: 2018_09_25_081406) do
     t.datetime "updated_at", null: false
     t.integer "guest_number"
     t.integer "total_bill"
-    t.boolean "paid", default: false
+    t.boolean "paid"
     t.index ["listing_id"], name: "index_reservations_on_listing_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
