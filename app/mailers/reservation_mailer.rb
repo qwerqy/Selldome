3df62916f617ad5d903d3f1df0cf307b07179400
qwerqy <amnrsln@gmail.com>
2018-09-30 02:53:46 +0800

    added multipart/alternative to reservmailer

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -6,6 +6,15 @@ class ReservationMailer < ApplicationMailer
     @host = host
     @reservation_id = reservation_id
     @url = 'http://selldome.com/home'
+    content_type "multipart/alternative"
+
+    part content_type: "text/html",
+      body: render_message("booking_email", email: @host.email)
+
+    part "text/plain" do |p|
+      p.body = render_message("booking_email_text", email: @host.email)
+      p.transfer_encoding = 'base64'
+    end
     mail(to: @host.email, subject: 'You have a reservation!')
   end
 end
