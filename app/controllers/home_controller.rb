d548bb651a166da55301f3f88d13ef9459067840
qwerqy <amnrsln@gmail.com>
2018-09-29 17:46:07 +0800

    home page 576 fixup 1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   before_action :require_login
 
   def index
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def load_content
