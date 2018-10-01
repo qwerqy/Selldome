214a86cd3d3e99be41c0d6e53f36b0c29cd33408
qwerqy <amnrsln@gmail.com>
2018-10-01 11:31:48 +0800

    configured production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.active_job.queue_adapter = :sidekiq
+
+  config.action_mailer.default_url_options = { host: 'selldome.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['GMAIL_USER_NAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
