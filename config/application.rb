ee94e21fbb437eeecf1ea51d7befbeac397cdfae
qwerqy <amnrsln@gmail.com>
2018-10-01 20:24:56 +0800

    environment for development

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Selldome
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.skylight.environments += ["production"]
+    config.skylight.environments += ["development"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
