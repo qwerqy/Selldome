d8b428c5438ad7d36eb67590b458708a403863dc
qwerqy <amnrsln@gmail.com>
2018-10-01 11:41:23 +0800

    reverted refactor . AJAX call couldnt be detected

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -92,6 +92,22 @@ class ListingsController < ApplicationController
     end
   end
 
+  def property_from_place_type
+    place = params[:place_type]
+    if place == '1'
+      @property_type = ["Apartment", "Condominium", "Casa particular (Cuba)", "Loft", "Serviced Apartment"]
+    elsif place == '2'
+      @property_type = ["House", "Bungalow", "Cabin", "Casa particular (Cuba)", "Chalet", "Cottage", "Cycladic House", "Dammuso (Italy)", "Dome House", "Earth House", "Farmstay", "Houseboat", "Hut", "Lighthouse", "Pension (South Korea)", "Shepherd's Hut (UK, France)", "Tiny House", "Townhouse", "Trullo (Italy)", "Villa"]
+    elsif place == '3'
+      @property_type = ["Guesthouse","Guest Suite","Farmstay"]
+    elsif place == '4'
+      @property_type = ["Barn", "Boat", "Bus", "Camper/RV", "Campsite", "Castle", "Cave", "Dome House", "Earth House", "Farmstay", "Hut", "Igloo, Island", "Lighthouse", "Pension (South Korea)", "Plane", "Shepherd's Hut (UK, France)", "Tent", "Tinyhouse", "Tipi", "Train", "Treehouse", "Windmill", "Yurt"]
+    elsif place == '5'
+      @property_type = ["Boutique Hotel", "Aparthotel", "Heritage Hotel (India)", "Hostel", "Hotel", "Natural Lodge", "Resort", "Serviced Apartment"]
+    end
+    render json: {property_type: @property_type}
+  end
+
   private
 
   def listing_params
