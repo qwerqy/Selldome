ef3c450177a78fcc4f6b6d91b1285c7bd6a6cfd0
qwerqy <amnrsln@gmail.com>
2018-10-02 22:08:56 +0800

    redir to root after sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -41,10 +41,11 @@ class SessionsController < Clearance::SessionsController
       end
     end
 
-    def destroy
-      sign_out
-      redirect_to root_path
-    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
   end
 
 end
