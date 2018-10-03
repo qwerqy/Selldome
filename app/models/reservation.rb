7adf10115f76b2b20b329fa3e40c22fde0cf9e06
qwerqy <amnrsln@gmail.com>
2018-10-03 14:54:50 +0800

    whitespace remove

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,24 +4,18 @@ class Reservation < ApplicationRecord
   belongs_to :user
   delegate :id, :avatar, :name, :first_name, :last_name, :birthday, :email, :phone, :country, :gender, :about_me, to: :user, prefix: true
   delegate :name, to: :listing, prefix: true
-  
   validates :start_time , presence: true
   validates :end_time , presence: true
   validates :listing_id , uniqueness: { scope: :user,
     message: "You can't reserve a Home twice!" }
   validate :no_reservation_overlap, unless: :skip_validations
-
     scope :overlapping, ->(period_start, period_end) do
       where "((start_time <= ?) and (end_time >= ?))", period_end, period_start
     end
-
   def days
     "#{self.end_time.day - self.start_time.day} nights"
   end
-
-
   private
-
   def no_reservation_overlap
     if (Reservation.overlapping(start_time, end_time).any?)
        errors.add(:end_time, 'it overlaps another reservation')
